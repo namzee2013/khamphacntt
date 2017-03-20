8e0eac439a3412fbbf93a0f69e97fb700f29115b
Thanh Nam <nam.vboys@gmail.com>
2017-03-20 16:33:52 +0700

    +add menu

---
+++ packages/custom/webs/public/controllers/nav.js
@@ -0,0 +1,39 @@
+(function() {
+    'use strict';
+
+    /* jshint -W098 */
+
+    function NavController($scope, $rootScope, Global, Nav, $stateParams, $state, $location, $http) {
+        $scope.global = Global;
+        $scope.package = {
+            name: 'nav'
+        };
+
+        $scope.getAllCate = function(){
+
+          Nav.getCategoryNotNull().then(function(response){
+            if(response.status == 200){
+              $scope.categories = response.data;
+            }
+          })
+          // Nav.getParentCategory().then(function(response){
+          //   var categories = response.data;
+          //   angular.forEach(categories, function(value, key){
+          //       Nav.getCategoryByParent(value._id).then(function(response){
+          //           value.parent.push(response.data);
+          //       });
+          //   }, this);
+          //   $scope.categories = categories;
+          //   console.log($scope.categories);
+          // });
+
+        }
+    }
+
+    angular
+        .module('mean.webs')
+        .controller('NavController', NavController);
+
+    NavController.$inject = ['$scope', '$rootScope', 'Global', 'Nav', '$stateParams', '$state', '$location', '$http'];
+
+})();
---
+++ packages/custom/webs/public/services/nav.js
@@ -0,0 +1,52 @@
+(function() {
+    'use strict';
+
+    function Nav($http, $q) {
+        return {
+            getParentCategory: function(){
+              var deferred = $q.defer();
+              $http({
+                method: 'GET',
+                url: '/api/parent'
+              }).then(function successCallback(response) {
+                deferred.resolve(response);
+              }, function errorCallback(response) {
+                deferred.reject(response);
+              });
+              return deferred.promise;
+            },
+            getCategoryByParent: function(parent_id){
+              var deferred = $q.defer();
+              $http({
+                method: 'GET',
+                url: '/api/category/parent/' + parent_id
+              }).then(function successCallback(response) {
+                deferred.resolve(response);
+              }, function errorCallback(response) {
+                deferred.reject(response);
+              });
+              return deferred.promise;
+            },
+            getCategoryNotNull: function(){
+              var deferred = $q.defer();
+              $http({
+                method: 'GET',
+                url: '/api/parent/notnull'
+              }).then(function successCallback(response) {
+                deferred.resolve(response);
+              }, function errorCallback(response) {
+                deferred.reject(response);
+              });
+              return deferred.promise;
+            }
+        };
+    }
+
+
+    angular
+        .module('mean.webs')
+        .factory('Nav', Nav);
+
+    Nav.$inject = ['$http', '$q'];
+
+})();
