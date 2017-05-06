6d44b1d79ff1d7b27e718792a01289fe7be3f1c0
namzee2013 <nam.vboys@gmail.com>
2017-05-06 10:00:43 +0700

    script

--- custom_libs/js/custom-scripts.js
+++ custom_libs/js/custom-scripts.js
@@ -9,10 +9,10 @@
     var mainApp = {
 
         initFunction: function () {
-            /*MENU 
+            /*MENU
             ------------------------------------*/
             $('#main-menu').metisMenu();
-			
+
             $(window).bind("load resize", function () {
                 if ($(this).width() < 768) {
                     $('div.sidebar-collapse').addClass('collapse')
@@ -181,8 +181,8 @@
                 hideHover: 'auto',
                 resize: true
             });
-           
-     
+
+
         },
 
         initialization: function () {
@@ -195,6 +195,33 @@
 
     $(document).ready(function () {
         mainApp.initFunction();
+    });
+    
+    $(function() {
+    	$('#main-menu').smartmenus({
+    		subMenusSubOffsetX: 1,
+    		subMenusSubOffsetY: -8
+    	});
+    	$('#main-menu').smartmenus('keyboardSetHotkey', 123, 'shiftKey');
+    });
+
+    $(document).ready(function () {
+    	var stickyNavTop = $('#main-nav').offset().top;
+
+    	var stickyNav = function () {
+    		var scrollTop = $(window).scrollTop();
+    		if (scrollTop > stickyNavTop) {
+    			$('#main-nav').addClass('navbar-fixed-top');
+    		} else {
+    			$('#main-nav').removeClass('navbar-fixed-top');
+    		}
+    	};
+
+    	stickyNav();
+
+    	$(window).scroll(function () {
+    		stickyNav();
+    	});
     });
 
 }(jQuery));
