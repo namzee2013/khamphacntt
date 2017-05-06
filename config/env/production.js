d438eaf224cfe18faf0581ce55d74e65bacb3a4d
namzee2013 <nam.vboys@gmail.com>
2017-05-06 09:17:28 +0700

    make it better

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict'
 
 module.exports = {
-  db: 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') + '/khamphacntt-prod',
+  db: 'mongodb://thanhnam:1@ds119020.mlab.com:19020/khamphacntt',
   /**
    * Database options that will be passed directly to mongoose.connect
    * Below are some examples.
