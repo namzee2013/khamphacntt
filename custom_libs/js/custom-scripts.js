ada94983629e06cc75d4429fa80dcb3c6573193f
namzee2013 <nam.vboys@gmail.com>
2017-05-06 10:03:24 +0700

    script

--- custom_libs/js/custom-scripts.js
+++ custom_libs/js/custom-scripts.js
@@ -196,32 +196,7 @@
     $(document).ready(function () {
         mainApp.initFunction();
     });
+
     
-    $(function() {
-    	$('#main-menu').smartmenus({
-    		subMenusSubOffsetX: 1,
-    		subMenusSubOffsetY: -8
-    	});
-    	$('#main-menu').smartmenus('keyboardSetHotkey', 123, 'shiftKey');
-    });
-
-    $(document).ready(function () {
-    	var stickyNavTop = $('#main-nav').offset().top;
-
-    	var stickyNav = function () {
-    		var scrollTop = $(window).scrollTop();
-    		if (scrollTop > stickyNavTop) {
-    			$('#main-nav').addClass('navbar-fixed-top');
-    		} else {
-    			$('#main-nav').removeClass('navbar-fixed-top');
-    		}
-    	};
-
-    	stickyNav();
-
-    	$(window).scroll(function () {
-    		stickyNav();
-    	});
-    });
 
 }(jQuery));
